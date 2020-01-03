8a4f83a952ff0da6b7e8f6f852b1c669236825d9
w1nsun <ilya.nevmyvaka@gmail.com>
2020-01-03 23:49:33 +0200

    handle errors

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,27 +1,24 @@
 package users
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/w1nsun/bookstore_users-api/domain/users"
 	"github.com/w1nsun/bookstore_users-api/services"
+	"github.com/w1nsun/bookstore_users-api/utils/errors"
 	"net/http"
-)
-
-var (
-	counter int
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		fmt.Println(err)
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
-		//TODO: Handle creation user
+		c.JSON(saveErr.Status, saveErr)
 		return
 	}
 
--- services/users_service.go
+++ services/users_service.go
@@ -1,7 +1,17 @@
 package services
 
-import "github.com/w1nsun/bookstore_users-api/domain/users"
+import (
+	"github.com/w1nsun/bookstore_users-api/domain/users"
+	"github.com/w1nsun/bookstore_users-api/utils/errors"
+	"net/http"
+)
 
-func CreateUser(user users.User) (*users.User, error) {
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	return &user, nil
+
+	return nil, &errors.RestErr{
+		Message: "invalid json body",
+		Status:  http.StatusInternalServerError,
+		Error:   "bad_request",
+	}
 }
