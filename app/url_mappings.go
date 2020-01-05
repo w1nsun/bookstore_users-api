d2f86906a1ddeb78a738d5994f8f57ad0ca2dfe8
w1nsun <ilya.nevmyvaka@gmail.com>
2020-01-05 21:50:13 +0200

    update user

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -8,7 +8,8 @@ import (
 func mapUrls() {
 	router.GET("/ping", ping.Ping)
 
-	router.GET("/users/:user_id", users.GetUser)
-	//router.GET("/users/search", controllers.SearchUser)
 	router.POST("/users", users.CreateUser)
+	router.GET("/users/:user_id", users.GetUser)
+	router.PUT("/users/:user_id", users.UpdateUser)
+	router.PATCH("/users/:user_id", users.UpdateUser)
 }
